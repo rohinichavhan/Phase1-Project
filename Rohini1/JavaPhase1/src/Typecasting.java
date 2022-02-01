ea57bedd391aa89b27312d38c57358d13a1b2db4
Rohini Chavhan <98108512+rohinichavhan@users.noreply.github.com>
2022-02-01 22:39:13 +0530

    This is My First Type Casting Project.

---
+++ Rohini1/JavaPhase1/src/Typecasting.java
@@ -0,0 +1,15 @@
+
+public class Typecasting {
+	
+	    public static void main(String[] args) {
+	       String str = "100";
+	       System.out.println(Integer.valueOf(str) + ": This is a Integer Value");
+	       System.out.println(Boolean.valueOf(str) + ": This is a Boolean Value");
+	       System.out.println(Long.valueOf(str) + ": This is a Long  Value");
+	       System.out.println(Float.valueOf(str) + ": This is a Float  Value");
+	       System.out.println(Double.valueOf(str) + ": This is a Double Value");
+	       System.out.println(Short.valueOf(str) + ": This a is Short Value");
+	       System.out.println(Byte.valueOf(str) + ": This is a Byte Value");
+
+	    }
+	}
